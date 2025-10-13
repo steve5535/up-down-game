62724a7c2dc3a6fe7a5b8ec93b1a03575a6e7153
성선혁 <steve5535@naver.com>
2025-10-13 22:57:52 +0900

    feat(game): 시도 횟수 추가

--- main_pygame.py
+++ main_pygame.py
@@ -6,7 +6,8 @@ pygame.init() # 초기화
 screen = pygame.display.set_mode((600,400)) # 화면 크기 설정
 pygame.display.set_caption("Up-Down Game (Pygame)") # 창 이름 설정
 
-answer = random.randrange(1, 101) # 난수로 답을 저장
+answer = random.randrange(1, 101) # 난수로 답을 변수에 저장
+count = 0 # 시도 횟수 변수
 
 user_input = "" # 유저 입력을 받는 변수
 message = "" # 메시지 초기화
@@ -22,6 +23,7 @@ while True: # 화면에 표시하는 메인 루프
             if event.key == pygame.K_RETURN: # 업다운 게임 메인 루프
                 try:
                     guess = int(user_input)
+                    count += 1 # 시도 횟수 증가
                     if guess < answer:
                         message = "업"
                     elif guess > answer:
@@ -39,10 +41,12 @@ while True: # 화면에 표시하는 메인 루프
                 user_input += event.unicode
 
 
-    text = font.render(user_input, True, (255, 255, 255)) # 화면에 입력값 렌더링
+    text = font.render(user_input, True, (255, 255, 255)) # 화면에 입력값 출력
     screen.blit(text, (200, 150))
 
-    message_text = font.render(message, True, (255, 255 ,0)) # 업다운 판별값 렌더링
+    message_text = font.render(message, True, (255, 255 ,0)) # 업다운 판별값 출력
     screen.blit(message_text, (200, 250))
+    count_text = font.render(f"시도 횟수 : {count}", True, (200,200,200)) # 시도 횟수 화면에 출력
+    screen.blit(count_text, (100, 50))
 
     pygame.display.update()
